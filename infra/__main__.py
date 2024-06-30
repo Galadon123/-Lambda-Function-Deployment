ea5a01bfe937acd193fdeeb753a9813308d4fd0e
Fazlul Karim <fazlulkarim362@gmail.com>
2024-06-30 20:23:10 +0600

    fir

--- infra/__main__.py
+++ infra/__main__.py
@@ -30,6 +30,7 @@ public_subnet = aws.ec2.Subnet("public-subnet",
                                vpc_id=vpc.id,
                                cidr_block="10.0.1.0/24",
                                availability_zone="us-east-1a",
+                               map_public_ip_on_launch=True,
                                opts=pulumi.ResourceOptions(depends_on=[vpc]),
                                tags={"Name": "public-subnet"})
 
