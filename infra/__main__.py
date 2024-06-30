f340c574362aa22758aae60d380a61d8ede8e618
Fazlul Karim <fazlulkarim362@gmail.com>
2024-06-30 19:39:12 +0600

    fasf

--- infra/__main__.py
+++ infra/__main__.py
@@ -1,24 +1,101 @@
-from vpc import VPC
-from subnet import PublicSubnet
-from ecr_repository import ECRRepository
-from lambda_role import LambdaRole
-from security_group import SecurityGroup
-from ec2_instance import EC2Instance
+import pulumi
+import pulumi_aws as aws
 
-# Create the VPC and related resources
-vpc = VPC("my-vpc", "10.0.0.0/16")
+# Create VPC
+vpc = aws.ec2.Vpc("my-vpc",
+                  cidr_block="10.0.0.0/16",
+                  tags={"Name": "my-vpc"})
 
-# Create public subnet
-public_subnet = PublicSubnet("public-subnet", vpc.vpc.id, "10.0.1.0/24", "us-east-1a")
+# Create Internet Gateway
+igw = aws.ec2.InternetGateway("my-vpc-igw",
+                              vpc_id=vpc.id,
+                              tags={"Name": "my-vpc-igw"})
 
-# Create ECR repository
-ecr_repo = ECRRepository("my-lambda-function")
+# Create Route Table
+route_table = aws.ec2.RouteTable("my-vpc-public-rt",
+                                 vpc_id=vpc.id,
+                                 routes=[{
+                                     "cidr_block": "0.0.0.0/0",
+                                     "gateway_id": igw.id,
+                                 }],
+                                 tags={"Name": "my-vpc-public-rt"})
 
-# Create Lambda role
-lambda_role = LambdaRole("lambda-role")
+# Create Public Subnet
+public_subnet = aws.ec2.Subnet("public-subnet",
+                               vpc_id=vpc.id,
+                               cidr_block="10.0.1.0/24",
+                               availability_zone="us-east-1a",
+                               tags={"Name": "public-subnet"})
 
-# Create security group
-lambda_security_group = SecurityGroup("lambda-security-group", vpc.vpc.id)
+# Associate Route Table with Subnet
+route_table_association = aws.ec2.RouteTableAssociation("public-subnet-association",
+                                                        subnet_id=public_subnet.id,
+                                                        route_table_id=route_table.id)
 
-# Create EC2 instance in public subnet for Grafana Tempo
-ec2_instance = EC2Instance("grafana-tempo", public_subnet.subnet.id, lambda_security_group.security_group.id)
+# Create Security Group
+security_group = aws.ec2.SecurityGroup("lambda-security-group",
+                                       vpc_id=vpc.id,
+                                       description="Allow HTTP inbound traffic",
+                                       ingress=[{
+                                           "protocol": "tcp",
+                                           "from_port": 80,
+                                           "to_port": 80,
+                                           "cidr_blocks": ["0.0.0.0/0"],
+                                       }],
+                                       tags={"Name": "lambda-security-group"})
+
+# Create ECR Repository
+ecr_repo = aws.ecr.Repository("my-lambda-function",
+                              image_scanning_configuration={"scanOnPush": True},
+                              tags={"Name": "my-lambda-function"})
+
+# Create IAM Role for Lambda
+lambda_role = aws.iam.Role("lambda-role",
+                           assume_role_policy="""{
+                               "Version": "2012-10-17",
+                               "Statement": [
+                                   {
+                                       "Action": "sts:AssumeRole",
+                                       "Principal": {
+                                           "Service": "lambda.amazonaws.com"
+                                       },
+                                       "Effect": "Allow",
+                                       "Sid": ""
+                                   }
+                               ]
+                           }""")
+
+# Attach Policy to Role
+s3_policy = aws.iam.Policy("s3Policy",
+                           policy="""{
+                               "Version": "2012-10-17",
+                               "Statement": [
+                                   {
+                                       "Effect": "Allow",
+                                       "Action": "s3:GetObject",
+                                       "Resource": "arn:aws:s3:::lambda-function-bucket-poridhi/pulumi-outputs.json"
+                                   }
+                               ]
+                           }""")
+
+lambda_role_policy_attachment = aws.iam.RolePolicyAttachment("lambdaRolePolicyAttachment",
+                                                             role=lambda_role.name,
+                                                             policy_arn=s3_policy.arn)
+
+# Create EC2 Instance for Grafana Tempo
+ec2_instance = aws.ec2.Instance("grafana-tempo",
+                                instance_type="t2.micro",
+                                ami="ami-0c55b159cbfafe1f0",  # Amazon Linux 2 AMI (HVM), SSD Volume Type
+                                subnet_id=public_subnet.id,
+                                vpc_security_group_ids=[security_group.id],
+                                tags={"Name": "grafana-tempo"})
+
+# Export outputs
+pulumi.export("vpc_id", vpc.id)
+pulumi.export("igw_id", igw.id)
+pulumi.export("public_subnet_id", public_subnet.id)
+pulumi.export("route_table_id", route_table.id)
+pulumi.export("ec2_instance_id", ec2_instance.id)
+pulumi.export("ecr_registry", ecr_repo.registry_id)
+pulumi.export("lambda_role_arn", lambda_role.arn)
+pulumi.export("ec2_instance_public_ip", ec2_instance.public_ip)
