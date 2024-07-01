0cff6a2fe6a8fbd82e39e1707a6d5c19660fa377
Fazlul Karim <fazlulkarim362@gmail.com>
2024-07-02 01:04:04 +0600

    fasfas

--- deploy-in-lambda/index.js
+++ deploy-in-lambda/index.js
@@ -3,12 +3,10 @@ const { OTLPTraceExporter } = require('@opentelemetry/exporter-otlp-grpc');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { trace, context } = require('@opentelemetry/api');
 const grpc = require('@grpc/grpc-js');
-const AWS = require('aws-sdk');
 const express = require('express');
 const awsServerlessExpress = require('aws-serverless-express');
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
 
-const s3 = new AWS.S3();
 const app = express();
 const server = awsServerlessExpress.createServer(app);
 
@@ -17,22 +15,12 @@ app.use(awsServerlessExpressMiddleware.eventContext());
 
 let otelInitialized = false;
 
-async function getEC2InstancePrivateIP(bucket, key) {
-  const params = { Bucket: bucket, Key: key };
-  const data = await s3.getObject(params).promise();
-  const outputs = JSON.parse(data.Body.toString('utf-8'));
-  return outputs.ec2_instance_private_ip; // Use the private IP of the EC2 instance
-}
-
 async function initializeOpenTelemetry() {
   if (!otelInitialized) {
     console.log("Initializing OpenTelemetry...");
     try {
-      const ec2InstancePrivateIP = await getEC2InstancePrivateIP('lambda-function-bucket-poridhi', 'pulumi-outputs.json');
-      console.log(`Fetched EC2 instance private IP: ${ec2InstancePrivateIP}`);
-
       const traceExporter = new OTLPTraceExporter({
-        url: `http://${ec2InstancePrivateIP}:4317`,
+        url: 'http://localhost:4317',  // Replace with your collector's URL
         credentials: grpc.credentials.createInsecure(),
       });
 
